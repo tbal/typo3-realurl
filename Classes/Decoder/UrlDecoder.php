tbal/typo3-realurl

commit b0945b0ed98913b2cb8a78053900a8f05ae54ee1

[TASK] #206: update code - set new GET parameters to $_GET, not to TSFE

Classes/Decoder/UrlDecoder.php
@@ -1192,22 +1192,22 @@ class UrlDecoder extends EncodeDecoderBase implements SingletonInterface {
 	}
 
 	/**
-	 * Merges $_GET from domains configuration if corresponding _GET parameter is
-	 * not set already.
+	 * Merges $_GET from domains configuration.
 	 */
 	protected function mergeGetVarsFromDomainsConfiguration() {
 		// Convert the configuration into an $_GET-"friendly" format
-		$getVars = $this->makeRealPhpArrayFromRequestVars($this->configuration->getGetVarsToSet());
+		$getVarsToSet = $this->makeRealPhpArrayFromRequestVars($this->configuration->getGetVarsToSet());
+		if (count($getVarsToSet) > 0) {
+			// Overwrite with $_GET-params that $_GET-parmas have a "higher" priority
+			$getVars = GeneralUtility::_GET();
+			if (!is_array($getVars)) {
+				$getVars = array();
+			}
+			ArrayUtility::mergeRecursiveWithOverrule($getVars, $getVarsToSet, true, true, false);
 
-		// Over-write with $_GET-params that $_GET-parmas have a "higher" priority
-		$GET = GeneralUtility::_GET();
-		if (!is_array($GET)) {
-			$GET = array();
+			// Store the "new" $_GET-params back
+			GeneralUtility::_GETset($getVars);
 		}
-		ArrayUtility::mergeRecursiveWithOverrule($getVars, $GET, TRUE, TRUE, FALSE);
-
-		// Store the "new" $_GET-params back
-		$this->caller->mergingWithGetVars($getVars);
 	}
 
 	/**

commit 4e77ff3ee7e8f9dff2206c51ab3e9b9d71dbae0f

[BUGFIX] Ensure higher priority of $_GET-vars
Correct the merge order of variables from $_GET and GETvars from domains configuration. $_GET must have the higher priority compared to GETvars.

The wrong order was introduced in b0945b0.

Classes/Decoder/UrlDecoder.php
@@ -1274,7 +1274,7 @@ class UrlDecoder extends EncodeDecoderBase implements SingletonInterface {
 			if (!is_array($getVars)) {
 				$getVars = array();
 			}
-			ArrayUtility::mergeRecursiveWithOverrule($getVars, $getVarsToSet, true, true, false);
+			ArrayUtility::mergeRecursiveWithOverrule($getVarsToSet, $getVars, true, true, false);
 
 			// Store the "new" $_GET-params back
 			GeneralUtility::_GETset($getVars);